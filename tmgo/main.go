db53e3b5c7fb3641ee9a779487b5c2732d6be666
tiannian <dtiannian@aliyun.com>
2021-07-11 04:53:13 +0800

    format go code

--- tmgo/main.go
+++ tmgo/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"github.com/spf13/viper"
 	cfg "github.com/tendermint/tendermint/config"
+	tmflags "github.com/tendermint/tendermint/libs/cli/flags"
 	"github.com/tendermint/tendermint/libs/log"
 	nm "github.com/tendermint/tendermint/node"
 	"github.com/tendermint/tendermint/p2p"
 	"github.com/tendermint/tendermint/privval"
 	"github.com/tendermint/tendermint/proxy"
 	"os"
+	"path/filepath"
 	"sync"
 	"unsafe"
-    "fmt"
-    "github.com/spf13/viper"
-    "path/filepath"
-    tmflags "github.com/tendermint/tendermint/libs/cli/flags"
 )
 
 /*
@@ -32,32 +32,32 @@ var nodes = make(map[int]*nm.Node)
 
 //export new_node
 func new_node(config_c C.ByteBuffer, abci_ptr unsafe.Pointer, userdata unsafe.Pointer) C.int32_t {
-    configFile := string(C.GoBytes(unsafe.Pointer(config_c.data), C.int(config_c.len)))
-    config := cfg.DefaultConfig()
+	configFile := string(C.GoBytes(unsafe.Pointer(config_c.data), C.int(config_c.len)))
+	config := cfg.DefaultConfig()
 
-    config.RootDir = filepath.Dir(filepath.Dir(configFile))
-    viper.SetConfigFile(configFile)
-    if err := viper.ReadInConfig(); err != nil {
-        fmt.Fprintf(os.Stderr, "viper failed to read config file: %v\n", err)
-        return -1
-    }
-    if err := viper.Unmarshal(config); err != nil {
-        fmt.Fprintf(os.Stderr, "viper failed to unmarshal config: %v\n", err)
-        return -1
-    }
-    if err := config.ValidateBasic(); err != nil {
-        fmt.Fprintf(os.Stderr, "config is invalid: %v\n", err)
-        return -1
-    }
+	config.RootDir = filepath.Dir(filepath.Dir(configFile))
+	viper.SetConfigFile(configFile)
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Fprintf(os.Stderr, "viper failed to read config file: %v\n", err)
+		return -1
+	}
+	if err := viper.Unmarshal(config); err != nil {
+		fmt.Fprintf(os.Stderr, "viper failed to unmarshal config: %v\n", err)
+		return -1
+	}
+	if err := config.ValidateBasic(); err != nil {
+		fmt.Fprintf(os.Stderr, "config is invalid: %v\n", err)
+		return -1
+	}
 
 	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
 
-    var err error
-    logger, err = tmflags.ParseLogLevel(config.LogLevel, logger, cfg.DefaultLogLevel())
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "failed to parse log level: %v\n", err)
-        return -4
-    }
+	var err error
+	logger, err = tmflags.ParseLogLevel(config.LogLevel, logger, cfg.DefaultLogLevel())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to parse log level: %v\n", err)
+		return -4
+	}
 
 	pv := privval.LoadFilePV(
 		config.PrivValidatorKeyFile(),
@@ -67,10 +67,9 @@ func new_node(config_c C.ByteBuffer, abci_ptr unsafe.Pointer, userdata unsafe.Po
 	nodeKey, err := p2p.LoadNodeKey(config.NodeKeyFile())
 	if err != nil {
 		// load node key error.
-        fmt.Fprintf(os.Stderr, "%v", err)
+		fmt.Fprintf(os.Stderr, "%v", err)
 		return -2
 	}
-
 
 	// Get index
 	mu.Lock()
@@ -83,7 +82,6 @@ func new_node(config_c C.ByteBuffer, abci_ptr unsafe.Pointer, userdata unsafe.Po
 
 	app := NewABCFApplication(abci_ptr, index, userdata)
 
-
 	node, err := nm.NewNode(
 		config,
 		pv,
@@ -95,7 +93,7 @@ func new_node(config_c C.ByteBuffer, abci_ptr unsafe.Pointer, userdata unsafe.Po
 		logger)
 
 	if err != nil {
-        fmt.Fprintf(os.Stderr, "%v", err)
+		fmt.Fprintf(os.Stderr, "%v", err)
 		return -3
 	}
 
