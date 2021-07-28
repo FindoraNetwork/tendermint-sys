5fab5e5e7192c1d0730582cbb2189764db74d3fd
tiannian <dtiannian@aliyun.com>
2021-07-28 15:41:21 +0800

    add feature for cleveldb and go golevel

--- tendermint-sys/build.rs
+++ tendermint-sys/build.rs
@@ -2,17 +2,36 @@ use std::env;
 use std::process::Command;
 // use std::path::Path;
 
+#[derive(Debug, Eq, PartialEq)]
+pub enum LevelDB {
+    Cleveldb,
+    Goleveldb,
+}
+
 fn main() {
     println!("cargo:rerun-if-changed=tmgo/app.go");
     println!("cargo:rerun-if-changed=tmgo/main.go");
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=script/build.sh");
     let out_dir = env::var("OUT_DIR").unwrap();
     let code_dir = env::current_dir().unwrap();
     let go_dir = code_dir.join("tmgo");
 
-    let status = Command::new("../scripts/build.sh")
-        .arg(&format!("{}/libtmgo.a", out_dir))
-        .current_dir(go_dir)
+    let leveldb = match env::var("CARGO_FEATURE_CLEVELDB") {
+        Ok(_) => LevelDB::Cleveldb,
+        Err(_) => LevelDB::Goleveldb,
+    };
+
+    let mut command = Command::new("../scripts/build.sh");
+    if leveldb == LevelDB::Cleveldb {
+        command.arg("cleveldb");
+    } else {
+        command.arg("goleveldb");
+    }
+    command.arg(&format!("{}/libtmgo.a", out_dir));
+    command.current_dir(go_dir);
+
+    let status = command
         .status()
         .unwrap();
 
@@ -20,7 +39,10 @@ fn main() {
 
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static=tmgo");
-    println!("cargo:rustc-link-lib=leveldb");
+    
+    if leveldb == LevelDB::Cleveldb {
+        println!("cargo:rustc-link-lib=leveldb");
+    }
 
     let target = env::var("TARGET").unwrap();
     if target == "x86_64-apple-darwin" {
--- tendermint-sys/scripts/build.sh
+++ tendermint-sys/scripts/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-CGO_ENABLED=1 go build -buildmode=c-archive -tags cleveldb -ldflags '-s -w --extldflags "-static -fpic"' -o $1
-
+if [ "$1" = "cleveldb" ]; then
+    CGO_ENABLED=1 go build -buildmode=c-archive -tags cleveldb -ldflags '-s -w --extldflags "-static -fpic"' -o $2
+else
+    CGO_ENABLED=1 go build -buildmode=c-archive -ldflags '-s -w --extldflags "-static -fpic"' -o $2
+fi
