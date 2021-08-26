02d0e374f37e80d75cea13dd973fef07e086fed4
tiannian <dtiannian@aliyun.com>
2021-08-26 12:04:20 +0800

    add golevel and clevel compile option

--- tendermint-sys/build.rs
+++ tendermint-sys/build.rs
@@ -2,19 +2,36 @@ use std::env;
 use std::process::Command;
 // use std::path::Path;
 
+#[derive(Debug, Eq, PartialEq)]
+pub enum LevelDB {
+    Cleveldb,
+    Goleveldb,
+}
+
 fn main() {
     println!("cargo:rerun-if-changed=tmgo/app.go");
     println!("cargo:rerun-if-changed=tmgo/main.go");
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=script/build.sh");
     let out_dir = env::var("OUT_DIR").unwrap();
     let code_dir = env::current_dir().unwrap();
     let go_dir = code_dir.join("tmgo");
 
-    let status = Command::new("../scripts/build.sh")
-        .arg(&format!("{}/libtmgo.a", out_dir))
-        .current_dir(go_dir)
-        .status()
-        .unwrap();
+    let leveldb = match env::var("CARGO_FEATURE_CLEVELDB") {
+        Ok(_) => LevelDB::Cleveldb,
+        Err(_) => LevelDB::Goleveldb,
+    };
+
+    let mut command = Command::new("../scripts/build.sh");
+    if leveldb == LevelDB::Cleveldb {
+        command.arg("cleveldb");
+    } else {
+        command.arg("goleveldb");
+    }
+    command.arg(&format!("{}/libtmgo.a", out_dir));
+    command.current_dir(go_dir);
+
+    let status = command.status().unwrap();
 
     assert!(status.success());
 
